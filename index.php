43eccec5018554dde0f7e7d59028e3ef4ed8a534
x-tanaka <oda.nobunyagasama@gmail.com>
2019-03-13 11:10:22 +0900

    index.php作成

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+    <html>
+    <head>
+   <meta charset="UTF-8">
+   <title>PHP test</title>
+   </head>
+    
+    <body>
+        <h1>PHP TEST</h1>
+        <?php echo "<p>はろーPHP</p>"; ?>
+    </body>
+    </html>
