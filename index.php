e6383394d6d9bbe3b6062bab4e410dcfde9ca480
x-tanaka <oda.nobunyagasama@gmail.com>
2019-03-13 11:30:46 +0900

    index変更

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!doctype html>
-    <html>
-    <head>
-   <meta charset="UTF-8">
-   <title>PHP test</title>
-   </head>
-    
-    <body>
-        <h1>PHP TEST</h1>
-        <?php echo "<p>はろーPHP</p>"; ?>
-    </body>
-    </html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP test</title>
+</head>
+
+<body>
+    <h1>PHP TESTですと</h1>
+    <?php echo "<p>はろー、PHP</p>"; ?>
+    <p>こんなんでました。</p>
+</body>
+</html>
